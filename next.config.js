1a89ea49a47d979ec4c20e06b50ceb8fd631c831
Antareep Hasan <antareephasan@gmail.com>
2023-11-01 17:27:44 +0600

    home added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'res.cloudinary.com',
+            port: '',
+            pathname: '/**',
+          },
+        ],
+      },
+
+    // images: {
+    //     domains: [
+    //         'res.cloudinary.com'
+    //     ]
+    // }
+}
 
 module.exports = nextConfig
